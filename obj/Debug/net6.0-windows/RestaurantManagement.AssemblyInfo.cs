7da1badc799c558a45f0952ebf52a3a18e63f1b4
Văn Nam <vannamdang2003@gmail.com>
2024-04-01 22:02:43 +0700

    config

--- obj/Debug/net6.0-windows/RestaurantManagement.AssemblyInfo.cs
+++ obj/Debug/net6.0-windows/RestaurantManagement.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RestaurantManagement")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6f49d2c824f1c827145899d526b7caef0ade2a95")]
 [assembly: System.Reflection.AssemblyProductAttribute("RestaurantManagement")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RestaurantManagement")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
